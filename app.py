ca0ebf4acdcaca2a8937a96515f3da11c2fb7d49
Kadamb Kaluskar <kadamb.k@gmail.com>
2018-11-17 01:36:22 +0530

    Inching towards success

--- app.py
+++ app.py
@@ -12,44 +12,87 @@ movieObj = Movies()
 
 @app.route('/')
 def index():
-    return "Hello World"
+    response_str = '''Please try one of the following URL :
+                      to search GET /api/movies?param1=value1&param2=value2
+                      to get a movie GET /api/movies/movie_id
+                      to add a movie POST /api/movies
+                      to edit a movie PUT /api/movies/movie_id
+                      to delete a movie DELETE /api/movies/movie_id'''
+    return response_str
 
 
-@app.route('/api/search/movies')
+@app.route('/api/movies',methods=['GET'])
 def search_movie():
-    return "API for searching movie"
+    #first creating API only to search on the basis of movie name,director and genre
+    #add later searching based on imdb_score and 99_popularity, with lower than,greater than or equal to filter
+    #this api created only for searching based on filter, and would not return anything if filter is not provided
+    #if requried, if no params are sent, we can return first 10 results
+    param = {}
+    if request.args.get("movie_name") is not None :
+        param['movie_name'] = request.args.get("movie_name")
+    if request.args.get("movie_director") is not None :
+        param['movie_director'] = request.args.get("movie_director")
+    if request.args.get("movie_genre") is not None :
+        param['movie_genre'] = request.args.get("movie_genre")
+    if not param :
+       return jsonify({'message' : 'no filter criteria found'})
+    movie_response = movieObj.search_movie(param)
+    
+    return jsonify(movie_response)
 
 @app.route('/api/movies',methods=['POST'])
 def add_movie():
+    token_response = authObj.check_token(request)
+    if  token_response == 'Token Expired' or token_response == 'Invalid token' :
+        return jsonify({'message' : token_response}) 
+    if not is_admin(token_response) :
+        return jsonify({'error' : 'Unauhorized'}),403
+    
     request_param = request.get_json()
-    print request_param
     validation_response = movieObj.validate_add_movie_param(request_param)
     if validation_response : 
         return jsonify({'message' : validation_response})
-    return "API to add movies |  only access to admin"
+    movie_response = movieObj.add_movie(request_param)
+    return jsonify(movie_response)
+
 
 @app.route('/api/movies/<movie_id>',methods=['PUT'])
 def edit_movie(movie_id):
-    return "API to edit movies | only admin access"
+    token_response = authObj.check_token(request)
+    if  token_response == 'Token Expired' or token_response == 'Invalid token' :
+        return jsonify({'message' : token_response})
+    if not is_admin(token_response) : 
+        return jsonify({'error' : 'Unauthorized'}),403
+    
+    if not movie_id.isdigit() :
+        return jsonify({'error' : 'INvalid movie id'})
+    request_param = request.get_json()
+    validation_response = movieObj.validate_edit_movie_param(request_param)
+    if validation_response :
+        return jsonify(validation_response)
+    request_param['movie_id'] = movie_id
+    movie_response = movieObj.edit_movie(request_param)
+    
+    return jsonify(movie_response)
 
 @app.route('/api/movies/<movie_id>',methods=['DELETE'])
 def delete_movie(movie_id) :
-    #token_response = authObj.check_token(request)
-    #if  token_response == 'Token Expired' or token_response == 'Invalid token' :
-    #    return jsonify({'message' : token_response})
-    #if not is_admin(token_response) :
-    #    return jsonify({'error' : 'Unauthorized'}),403
-    #write code to delete movide
+    token_response = authObj.check_token(request)
+    if  token_response == 'Token Expired' or token_response == 'Invalid token' :
+        return jsonify({'message' : token_response})
+    if not is_admin(token_response) :
+        return jsonify({'error' : 'Unauthorized'}),403
     if not movie_id.isdigit():
         return jsonify({'error' : 'Invalid movie id'})
+
     movie_response = movieObj.delete_movie(movie_id)
     return jsonify(movie_response)
 
 @app.route('/api/movies/<movie_id>',methods=['GET'])
 def get_movie(movie_id):
-    #token_response = authObj.check_token(request)
-    #if  token_response == 'Token Expired' or token_response == 'Invalid token' :
-    #    return jsonify({'message' : token_response})
+    token_response = authObj.check_token(request)
+    if  token_response == 'Token Expired' or token_response == 'Invalid token' :
+        return jsonify({'message' : token_response})
     if not movie_id.isdigit():
         return jsonify({'error' : 'Invalid movie id'})
     movie_response = movieObj.get_movie(movie_id)
@@ -62,10 +105,6 @@ def get_movie(movie_id):
 def is_admin(token_response):
     return True if token_response['is_admin'] == 1 else False
 
-
-def login(username,password) : 
-    return True
-
 @app.route('/api/token',methods=['POST'])
 def get_auth_token():
     #content type should be application/json
@@ -76,10 +115,7 @@ def get_auth_token():
     login_response = authObj.login(username,password)
     if not login_response :
         return jsonify({'message' : 'Invalid username or password'})
-    print data
     token = authObj.encode_auth_token(login_response['user_id'],login_response['is_admin'])
-    print token
-    #write code to verify username and password and return auth token
     return jsonify({ 'token' : token})
 if __name__ == '__main__' :
     app.run(host='0.0.0.0')
--- models/Movies.py
+++ models/Movies.py
@@ -6,25 +6,104 @@ class Movies(DB_CLASS):
     global dbObj
     dbObj = DB_CLASS()
 
+
+    def validate_edit_movie_param(self,param) :
+        #to validate the param and to check that atleast one param is present
+        if ('movie_name' not in param and 
+            'movie_director' not in param and 
+            'movie_genre' not in param and
+            'movie_99_popularity' not in param and 
+            'movie_imdb_score' not in param) :
+            return {'message' : 'please pass atleast one field to update'}
+        return False
+
     def validate_add_movie_param(self,param) :
         missing_param = ''
-        if 'movie_name' not in param : 
+        if 'movie_name' not in param : #also add regex to allow only characters,number and ',' and space
             missing_param += 'movie_name,'
-        if 'movie_director' not in param :
+        if 'movie_director' not in param : #add regex to only allow characers and space
             missing_param += 'movie_director,'
-        if 'movie_99_popularity' not in param :
+        if 'movie_99_popularity' not in param : # add regex to only allow decimal and between 0 and 100 
             missing_param += 'movie_99_popularity,'
-        if ('movie_genre' not in param) and (type(param['movie_genre']) != list):
+        if ('movie_genre' not in param) and (type(param['movie_genre']) != list): 
             missing_param += 'movie_genre',
-        if 'movie_imdb_score' not in param :
+        if 'movie_imdb_score' not in param : #add regex to allow only decimal, in the range 0,10
             missing_param += 'movie_imdb_score,'
         if missing_param != '' :
             missing_param = missing_param.strip(',')
             missing_param = 'Following params are missing or invalid : ' + str(missing_param)
         return missing_param
 
-    def add_movie(self,params) :
-        pass
+
+    def edit_movie(self,param) :
+
+        try :
+            updateValueList = []
+            insertStr = ''
+            if 'movie_name' in param :
+                insertStr += 'movie_name = %s ,' 
+                updateValueList.append(param['movie_name'])
+            
+            if 'movie_director' in param :
+                insertStr += 'movie_director = %s,'
+                updateValueList.append(param['movie_director'])
+
+            if 'movie_99_popularity' in param :
+                insertStr += 'movie_99_popularity = %s,'
+                updateValueList.append(param['movie_99_popularity'])
+
+            if 'movie_imdb_score' in param :
+                insertStr += 'movie_imdb_score = %s,'
+                updateValueList.append(param['movie_imdb_score'])
+           
+            if 'movie_genre' in param :
+                movie_genre = param['movie_genre']
+                movie_genre = ','.join(movie_genre)
+                updateValueList.append(movie_genre)
+                insertStr += 'movie_genre = %s,'
+
+            insertStr = insertStr.strip(',')
+            updateValueList.append(param['movie_id'])
+            db = dbObj.connect_db()
+            cur = db.cursor()
+            cur.execute('UPDATE `movies` SET '+ insertStr + 'WHERE movie_id = %s',updateValueList)
+            db.commit()
+            if cur.rowcount :
+                return {'message' : 'movie updated successfully'}
+            else :
+                return {'message' : 'movie not found or coulnot be updated'}
+
+        except Exception as ins :
+            print ins
+            print traceback.print_exc()
+            return {'message' : 'movie couldnt not edited.please try again later.'}
+
+
+      
+
+    def add_movie(self,movie_param) :
+        try :
+            movie_director = movie_param['movie_director']
+            movie_name = movie_param['movie_name']
+            movie_99_popularity = movie_param['movie_99_popularity']
+            movie_imdb_score = movie_param['movie_imdb_score']
+            movie_genre = movie_param['movie_genre']
+            movie_genre = ','.join(movie_genre)
+        
+            db = dbObj.connect_db()
+            cur = db.cursor()
+            cur.execute('INSERT INTO `imdb_movie`.`movies` (`movie_id`, `movie_name`, `movie_director`, `movie_99_popularity`, `movie_genre`, `movie_imdb_score`) VALUES (NULL, %s, %s, %s, %s, %s)',[movie_name,movie_director,movie_99_popularity,movie_genre,movie_imdb_score])
+            db.commit()
+            if cur.rowcount :
+                movie_id = cur.lastrowid
+                return {'message' : 'movie added successfully' , 'movie_id' : movie_id}
+            else :
+                return {'message' : 'movie couldnot be added'}
+
+        except Exception as ins :
+            print ins
+            print traceback.print_exc()
+            return {'message' : 'Movie could not be added. Please try again later'}
 
     def delete_movie(self,movie_id) :
         try :
@@ -41,18 +120,17 @@ class Movies(DB_CLASS):
             print traceback.print_exc()
             return {'message' : 'Movie could not be deleted. Please try again later.'}
         
-    def edit_movie(self,movie_id) :
-        #write code to edit movie
-        pass
+
 
     def get_movie(self,movie_id):
-        #write code to get movie data
+        #to get movie data
         try :
             response = {}
             db = dbObj.connect_db()
             cur = db.cursor()
             cur.execute('SELECT * FROM movies WHERE movie_id = %s',movie_id)
             for row in cur :
+                response['movie_id'] = row[0]
                 response['movie_name'] = row[1]
                 response['movie_director'] = row[2]
                 response['movie_99_popularity'] = str(row[3])
@@ -65,6 +143,30 @@ class Movies(DB_CLASS):
             print traceback.exc()
             return {'message' : 'Movie could not be fetched. Please try again later'}
 
-    def search_movie(self,req):
-        #write code to search for a movie
-        pass
+    def search_movie(self,search_param):
+        try :
+            response = []
+            search_query = ''
+            search_values = []
+            for key in search_param :
+                search_query += ' '+key +' LIKE %s OR'
+                search_values.append('%' + str(search_param[key]) + '%')
+            search_query = search_query.strip("OR")
+            db = dbObj.connect_db()
+            cur = db.cursor()
+            cur.execute('SELECT `movie_id`,`movie_name`,`movie_director`,`movie_genre`,`movie_99_popularity`,`movie_imdb_score`  from movies WHERE ' + search_query , search_values)
+            print cur._last_executed
+            for row in cur :
+                movie = {}
+                movie['movie_id'] = row[0]
+                movie['movie_name'] = row[1]
+                movie['movie_director'] = row[2]
+                movie['movie_genre'] = row[3].split(',')
+                movie['movie_99_popularity'] = str(row[4])
+                movie['movie_imdb_score'] = str(row[5])
+                response.append(movie)
+            return response
+        except Exception as ins :
+            print ins
+            print traceback.print_exc()
+            return response
